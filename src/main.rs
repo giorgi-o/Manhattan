477a01b37b5d0c9f26b1a86bb9a8831d70d4c49f
Giorgio <20621396+giorgi-o@users.noreply.github.com>
2024-02-13 00:04:59 +0000

    Call Python hello_world() from Rust minimal proof of concept

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,40 @@
+use std::path::PathBuf;
+
+use pyo3::prelude::*;
+
 use logic::grid::Grid;
 
 mod logic {
-    pub mod grid;
     pub mod car;
-    pub mod pathfinding;
+    pub mod grid;
     pub mod passenger;
+    pub mod pathfinding;
 }
 
 mod render {
+    pub mod car;
+    pub mod grid;
+    pub mod passenger;
     pub mod render_main;
     pub mod util;
-    pub mod grid;
-    pub mod car;
-    pub mod passenger;
 }
 
+// fn main() {
+//     let grid = Grid::new();
+//     render::render_main::start(grid);
+// }
+
 fn main() {
-    let grid = Grid::new();
-    render::render_main::start(grid);
+    Python::with_gil(|py| {
+        // add ./python/src to sys.path
+        let cwd = std::env::current_dir().unwrap();
+        let src_dir = cwd.join("python").join("src");
+
+        let sys = py.import("sys").unwrap();
+        let path = sys.getattr("path").unwrap();
+        path.call_method("append", (src_dir,), None).unwrap();
+
+        let main = py.import("main").unwrap();
+        main.call_method("hello_world", (), None).unwrap();
+    });
 }
